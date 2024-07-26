6d48f1eb6cdacb26cc95926c7e2f284b5294f04a
keeko <787018661@qq.com>
2024-07-26 20:36:42 +0800

    [feat] 增加.less的全局声明文件

--- craco.config.js
+++ craco.config.js
@@ -1,18 +1,20 @@
 const CracoLessPlugin = require('craco-less');
 
-module.exports = {
-  plugins: [
-    {
-      plugin: CracoLessPlugin,
-      options: {
-        lessLoaderOptions: {
-          // 配置可以参照webpack的less-loader具体配置
-          lessOptions: {
-            javascriptEnabled: true, // 允许less文件中使用js表达式
-            math: 'always',
+module.exports = function () {
+  return {
+    plugins: [
+      {
+        plugin: CracoLessPlugin,
+        options: {
+          lessLoaderOptions: {
+            // 配置可以参照webpack的less-loader具体配置
+            lessOptions: {
+              javascriptEnabled: true, // 允许less文件中使用js表达式
+              math: 'always',
+            },
           },
         },
       },
-    },
-  ],
+    ],
+  };
 };
