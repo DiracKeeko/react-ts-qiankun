95b3c5ed1820d6003ae7491921c732e9aaa97df8
Keeko <787018661@qq.com>
2024-07-26 15:38:55 +0800

    [feat] 可用的react qiankun 基座

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Menu } from 'antd';
+import { VideoCameraOutlined } from '@ant-design/icons';
+import { NavLink } from 'react-router-dom';
+const { Header, Content, Footer } = Layout;
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Header
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <div className='demo-logo'>
+          <h1 style={{ color: '#fff' }}>名称</h1>
+        </div>
+        <Menu
+          theme='dark'
+          mode='horizontal'
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <VideoCameraOutlined />,
+              label: <NavLink to='/vue-micro1'>vue 1 应用</NavLink>,
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: <NavLink to='/react-micro1'>react 1 应用</NavLink>,
+            },
+          ]}
+        />
+      </Header>
+      <Content
+        style={{ height: 'calc(100vh - 128px)', backgroundColor: '#b4efe1' }}
+        id={'micro-app-container'}
+      >
+        <h1>主应用显示容器</h1>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>一个随意的footer</Footer>
+    </Layout>
   );
-}
-
+};
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
+import { registerMicroApps, start } from 'qiankun';
+
+import 'antd/dist/antd.css';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+registerMicroApps([
+  {
+    name: 'react-micro-application', // 子应用的名称，必须唯一。
+    entry: '//localhost:7777/subapp/sub-vue/', // 子应用项目本地运行地址
+    container: '#micro-app-container', //  子应用的容器（子应用嵌入到主项目id为container的容器）
+    activeRule: '/vue-micro1', // 子应用激活时的路由规则（子应用路由）
+    props: {
+      // 向子应用传参
+      state: false,
+      userId: 'userId',
+      token: '18145EWDFDEdsefEFFfdDFf145454',
+    },
+  },
+]);
 
+start();
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <BrowserRouter>
+    <ConfigProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ConfigProvider>
+  </BrowserRouter>,
+);
