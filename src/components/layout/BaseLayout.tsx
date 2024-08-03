8645ba2c26c4d8c813936c7c777d5e8cec5d24a9
Keeko <787018661@qq.com>
2024-08-03 19:23:29 +0800

    [feat] 两种基本Layout

---
+++ src/components/layout/BaseLayout.tsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { ConfigProvider, Layout } from 'antd';
+import zhCN from 'antd/lib/locale/zh_CN';
+import { Outlet } from 'react-router-dom';
+
+import { UserInfo } from '@/api/global/constant';
+
+import CusHeader from './CusHeader';
+import logo from '@/asset/icon_investment_promotion.svg';
+import '@/style/global.less';
+
+
+const { Content, Footer } = Layout;
+
+export const UserContext = React.createContext<UserInfo | undefined>(undefined);
+
+const BaseLayout: React.FC = () => {
+  const [user, setUser] = useState<UserInfo>();
+  useEffect(() => {
+    const oneUserInfo: UserInfo = {
+      userId: '007',
+      userName: '法外狂徒张三',
+      userAuthority: '1'
+    };
+    setUser(oneUserInfo);
+  }, []);
+  return (
+    <ConfigProvider locale={zhCN}>
+      <Layout>
+        <UserContext.Provider value={user}>
+          <CusHeader
+            icon={logo}
+            title='react-qiankun-main'
+            user={`${user?.userId}/${user?.userName}`}
+          />
+          <Layout className={'main'}>
+            <Content className={'content'}>
+              <Outlet />
+            </Content>
+            <Footer>一个随意的footer</Footer>
+          </Layout>
+        </UserContext.Provider>
+      </Layout>
+    </ConfigProvider>
+  );
+};
+
+export default BaseLayout;
